85e238cf033c8fffebed7b398c474c1ef00d3ffc
SHIVAM SHARMA <shivam691999@gmail.com>
2023-02-02 01:51:29 +0530

    add : cobra-cli (#1)
    * add : cobra-cli
    
    * fix : add errHandling for cmd.Execute

--- main.go
+++ main.go
@@ -1,32 +1,9 @@
 package main
 
-import (
-	"math/big"
-
-	"github.com/0xsharma/compact-chain/config"
-	"github.com/0xsharma/compact-chain/core"
-)
+import "github.com/0xsharma/compact-chain/cmd"
 
 func main() {
-	config := &config.Config{
-		ConsensusDifficulty: 16,
-		ConsensusName:       "pow",
-	}
-
-	chain := core.NewBlockchain(config)
-
-	chain.AddBlock([]byte("Block 1"))
-	chain.AddBlock([]byte("Block 2"))
-	chain.AddBlock([]byte("Block 3"))
-
-	currentNumber := int(chain.Current().Number().Int64())
-
-	for i := 0; i <= currentNumber; i++ {
-		block := chain.GetBlockByNumber(big.NewInt(int64(i)))
-		println("BlockNumber : ", block.Number().String())
-		println("BlockHash : ", block.Hash().String())
-		println("ParentHash : ", block.ParentHash().String())
-		println("BlockData : ", string(block.Data()))
-		println()
+	if err := cmd.Execute(); err != nil {
+		panic(err)
 	}
 }
